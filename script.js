b8d7f8088cd1c79c690da04a46a79098b8b181b2
Jack112-create <jackconroy777@gmail.com>
2021-04-27 19:24:20 +0100

    Animation added when key is pressed

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
   if (!audio) {
     return; // Stops function from running
   }
   audio.currentTime = 0; // Rewind audio to the start
   audio.play();
+  console.log(key);
+  key.classList.toggle("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
 });
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") {
+    return; // Skip if its not transform
+  }
+  this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
